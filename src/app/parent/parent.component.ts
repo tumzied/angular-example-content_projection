a04fbd9a1ff34746d3ed41b3c9683ad8854c8225
smmohiuddin-enosisbd <abdur.tumzied@enosisbd.com>
2022-04-29 05:56:28 +0600

    nc

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  items:number[]=[]
+  constructor() { }
+
+
+  ngOnInit(): void {
+  this.items=new Array(19).fill(1).map((i)=>i+1);
+  }
+
+}
